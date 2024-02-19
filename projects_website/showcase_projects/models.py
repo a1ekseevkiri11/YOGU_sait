c46ca1c5993bc624e49523da27ce632fff9ec351
a1ekseevkiri11 <alekseevkirill30092004@mail.ru>
2024-02-20 00:33:58 +0500

    Аутентификация

--- projects_website/showcase_projects/models.py
+++ projects_website/showcase_projects/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from .permission import(
+    canAddParticipation,
+    canAddProject
+)
 
 
 class Project(models.Model):
@@ -8,10 +12,35 @@ class Project(models.Model):
     place = models.IntegerField(default=0)
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def freePlaces(self):
+        return self.place > self.participation_set.count()
+
+    def studentInThisProject(self, user):
+        return self.participation_set.filter(student=user).exists()
+
+    def addStudent(self, user):
+        if not canAddParticipation(user):
+            return
+
+        if Participation.studentInProject(user):
+            return
+
+        if not self.freePlaces():
+            return
+
+        Participation.objects.create(project=self, student=user)
+
     def __str__(self):
         return self.title
     
 
+
 class Participation(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     student = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @staticmethod
+    def studentInProject(student):
+        return Participation.objects.filter(student=student).exists()
+    
+
--- projects_website/showcase_projects/views.py
+++ projects_website/showcase_projects/views.py
@@ -1,8 +1,13 @@
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.models import User
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, 
+    UserPassesTestMixin
+)
+from django.contrib.auth.models import (
+    User
+)
 from django.views.generic import (
     ListView,
     DetailView,
@@ -10,8 +15,17 @@ from django.views.generic import (
     UpdateView,
     DeleteView
 )
-from .models import Project, Participation
+from .models import (
+    Project, 
+    Participation
+)
 from .forms import ConfirmationForm
+from .permission import(
+    canAddParticipation,
+    canAddProject
+)
+
+
 
 class ProjectListView(ListView):
     model = Project
@@ -20,63 +34,44 @@ class ProjectListView(ListView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['showButtonCreateProject'] = isCustomer(self.request.user)
+        context['showButtonCreateProject'] = canAddProject(self.request.user)
         return context
     
+
 
 class ProjectDetailView(DetailView, UserPassesTestMixin):
     model = Project
     template_name = 'showcase_projects/project_detail.html'
     
-    
-    def test_func(self):
-        return isStudent(self.request.user)
-    
     def get_context_data(self, **kwargs):
+        project = self.get_object()
         context = super().get_context_data(**kwargs)
-        context['showButtonParticipationProject'] = isStudent(self.request.user) and freePlacesInProject(self.get_object())
+        context['participationProject'] = canAddParticipation(self.request.user) and project.freePlaces()
+        context['studentInProject'] = Participation.studentInProject(self.request.user)
+        context['studentInThisProject'] = project.studentInThisProject(self.request.user)
         return context
     
     def post(self, request, *args, **kwargs):
         project = self.get_object()
-        
-        if not isStudent(self.request.user):
-            return redirect('home')
-
         form = ConfirmationForm(request.POST)
         
         if form.is_valid():
-            addStudentInProject(self.request.user, project)
+            project.addStudent(self.request.user)
             form.cleaned_data['confirmation']
-            return redirect('project-detail', pk=project.pk)
 
-        return render(request, self.template_name, {'project': project, 'form': form})
-
-
-def freePlacesInProject(project):
-    if project.place > Participation.objects.filter(project=project).count():
-        return True
-    return False
+        return redirect('project-detail', pk=project.pk)
 
 
-def addStudentInProject(user, project):
-    if Participation.objects.filter(student=user):
-        return
-    
-    if not freePlacesInProject(project):
-        return
-    
-    Participation.objects.create(project=project, student=user)
-    
 
 class ProjectUserListView(ListView):
-    model =  Project
+    model = Project
     template_name = 'showcase_projects/project_user.html'
     context_object_name = 'projects'
 
     def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
         return Project.objects.filter(customer=user)
+
 
 
 class ProjectCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -89,9 +84,7 @@ class ProjectCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         return super().form_valid(form)
     
     def test_func(self):
-        if isCustomer(self.request.user):
-            return True
-        return False
+        return canAddProject(self.request.user)
 
 
 
@@ -106,10 +99,9 @@ class ProjectUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         post = self.get_object()
-        if self.request.user == post.customer:
-            return True
-        return False
+        return self.request.user == post.customer
     
+
 
 class ProjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Project
@@ -117,16 +109,4 @@ class ProjectDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         post = self.get_object()
-        if self.request.user == post.customer:
-            return True
-        return False
-
-
-def isCustomer(user):
-    return user.groups.filter(name='customer').exists()
-
-def isLecturer(user):
-    return user.groups.filter(name='lecturer').exists()
-
-def isStudent(user):
-    return user.groups.filter(name='student').exists()
+        return self.request.user == post.customer
